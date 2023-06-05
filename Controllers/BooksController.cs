54bd2757b5eb3c3dc4e9d5306aeb8c6d39765e2e
ekvosty <katya_vostrikova_2002@mail.ru>
2023-06-05 19:06:42 +0300

    initial commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VostrikovaLab.Models;
+
+namespace VostrikovaLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController
+    {
+        private static List<BookModel> _memCache = new List<BookModel>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BookModel>> Get()
+        {
+            return _memCache;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<BookModel> Get(int id)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Книга не найдена");
+
+            return _memCache[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] BookModel value)
+        {
+            _memCache.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] BookModel value)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Книга не найдена");
+
+            _memCache[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Книга не найдена");
+
+            _memCache.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VostrikovaLab.Models
+{
+    public class BookModel
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int PageCount { get; set; }
+    }
+}
