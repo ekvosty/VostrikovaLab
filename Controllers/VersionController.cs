b1a6cdce071f35d180d11b9c2948e116d8e02815
ekvosty <katya_vostrikova_2002@mail.ru>
2023-06-05 19:12:30 +0300

    added extended controller for version output

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using VostrikovaLab.Models;
+
+namespace VostrikovaLab.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var versionInfo = new VersionModel
+            {
+                Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+                Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
+                ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            };
+
+            return Ok(versionInfo);
+        }
+    }
+}
