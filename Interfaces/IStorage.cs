1b95d19a33edec611bbf54b8a828232a3c688134
ekvosty <katya_vostrikova_2002@mail.ru>
2023-06-05 20:02:23 +0300

    expand MemCache, create IStorage

---
+++ Interfaces/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using VostrikovaLab.Models;
+
+namespace VostrikovaLab.Interfaces
+{
+    public interface IStorage<T> where T : class
+    {
+        List<T> All { get; }
+        T this[Guid id] { get; set; }
+        void Add(T value);
+        void RemoveAt(Guid id);
+        bool Has(Guid id);
+    }
+
+}
---
+++ MemCache.cs
@@ -0,0 +1,61 @@
+﻿using VostrikovaLab.Interfaces;
+using VostrikovaLab.Models;
+
+namespace VostrikovaLab
+{
+    public class MemCache : IStorage<BookModel>
+    {
+        private object _sync = new object();
+        private List<BookModel> _memCache = new List<BookModel>();
+        public BookModel this[Guid id]
+        {
+            get
+            {
+                lock (_sync)
+                {
+                    if (!Has(id)) throw new Exception($"Нет книги с данным {id}");
+
+                    return _memCache.Single(x => x.Id == id);
+                }
+            }
+            set
+            {
+                if (id == Guid.Empty) throw new Exception("Нельзя получить книгу по пустому id");
+
+                lock (_sync)
+                {
+                    if (Has(id))
+                    {
+                        RemoveAt(id);
+                    }
+
+                    value.Id = id;
+                    _memCache.Add(value);
+                }
+            }
+        }
+
+        public System.Collections.Generic.List<BookModel> All => _memCache.Select(x => x).ToList();
+
+        public void Add(BookModel value)
+        {
+            if (value.Id != Guid.Empty) throw new Exception($"Нельзя добавить книгу с заданным id {value.Id}");
+
+            value.Id = Guid.NewGuid();
+            this[value.Id] = value;
+        }
+
+        public bool Has(Guid id)
+        {
+            return _memCache.Any(x => x.Id == id);
+        }
+
+        public void RemoveAt(Guid id)
+        {
+            lock (_sync)
+            {
+                _memCache.RemoveAll(x => x.Id == id);
+            }
+        }
+    }
+}
